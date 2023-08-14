5069e6f725dd0a52d2e03cfcc156dc460adaa6db
NoteeFX <edipriatama23@students.amikom.ac.id>
2023-08-14 12:31:04 +0700

    rsv

--- JS/song.js
+++ JS/song.js
@@ -1,4 +1,13 @@
- 
-    var musrik=document.getElementById("song");var player,playing=!1;function onYouTubeIframeAPIReady(){player=new YT.Player('video-yt',{height:'360',width:'640',playerVars:{'autoplay':0,'rel':0,'showinfo':0,'showsearch':0,'controls':1,'loop':1,'enablejsapi':1,'playlist':'QMForPSc-Mc'},videoId:'QMForPSc-Mc',events:{'onStateChange':onPlayerStateChange}})} 
-    function onPlayerStateChange(event){if(event.data==YT.PlayerState.PLAYING){musrik.pause();playing=!0}else if(event.data==YT.PlayerState.PAUSED){musrik.play();playing=!1}} 
+
+            var isYT = false;
+			jQuery( "#wdp-button-wrapper" ).on( "click", "button", function() {
+				if(document.body.contains(document.getElementById("song"))) {
+					document.getElementById("song").play();
+					isYT = false;
+				} else {
+					isYT = true;
+					player.playVideo();
+				}
+                
+            });
         
